6929b35e53168fb5c39c11578162ed477c526f8c
brysonstoner <blahblahg>
2018-02-12 13:49:33 -0700

    first commit

--- src/talkingHead.js
+++ src/talkingHead.js
@@ -8,7 +8,14 @@ class Song {
   getTitle(){
     return "title is: " + this.title + " by " + this.artist;
   }
+
 }
+
+var mySong1 = new Song("Talking Heads", "Psycho Killer");
+var mySong2 = new Song("Stevie Wonder", "Higher Ground");
+
+console.log(mySong1.getTitle());
+console.log(mySong2.getTitle());
 
 //Exercise:
 
@@ -17,3 +24,4 @@ class Song {
 //use the method you just created to log the title of the song. Then, 
 //create another instance of it with your favorite Stevie Wonder song and
 //log the title of the song. Export the class using the es6 export method.
+export {Song}
--- src/yourOwnClass.js
+++ src/yourOwnClass.js
@@ -1,2 +1,18 @@
 //create a class-subclass structure and implement it using javascript classes.
 //be sure each have at least one method on them.
+
+class spaceDawgz {
+
+    constructor (name, weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    getWeight() {
+        return "The dawg named " + this.name + " weighs " + this.weight + " lbs!!!"
+    }
+}
+
+let spaceDawg1 = new spaceDawgz("Pluto", 1000);
+
+console.log(spaceDawg1.getWeight());
